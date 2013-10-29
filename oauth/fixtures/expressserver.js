a84e8ecb2bb4ecc341c0dc8b300e496c08ddf9f0
Scott Ganyo <scott@ganyo.com>
2013-10-29 16:56:05 -0700

    fix require

--- oauth/fixtures/expressserver.js
+++ oauth/fixtures/expressserver.js
@@ -24,7 +24,7 @@
 "use strict";
 
 var express = require('express');
-var spi = require('.');
+var spi = require('../providers/apigee');
 var oauth = require('..');
 
 var config = require('../../common/testconfig-apigee');
