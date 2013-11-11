7053fe3128e05a283a1cf7da81a26b015478dc8e
Scott Ganyo <scott@ganyo.com>
2013-11-11 12:40:28 -0800

    simplify the intra-module linkage for Volos development

--- link.sh
+++ link.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
-npm link management/redis
-npm link management/apigee
-npm link oauth/common
-npm link oauth/redis
-npm link oauth/apigee
+# Run from the Volos root directory after running 'npm install'. Instead of using
+# 'node link' to install the various modules of Volos as global modules, this will
+# simply symlink the local modules in the Volos node_modules directory so all modules
+# under this directory can find each other.
+
+pushd node_modules
+ln -s ../management/apigee volos-management-apigee
+ln -s ../management/redis volos-management-redis
+
+ln -s ../oauth/common volos-oauth-common
+ln -s ../oauth/apigee volos-oauth-apigee
+ln -s ../oauth/redis volos-oauth-redis
+
+ln -s ../quota/common volos-quota-common
+ln -s ../quota/apigee volos-quota-apigee
+ln -s ../quota/memory volos-quota-memory
+
+ln -s ../cache volos-cache-memory
+popd
