2cf90996117f9fc2961afa10977353a0b9160d8f
Gregory Brail <greg@apigee.com>
2014-08-05 12:01:17 -0700

    Fix some problems with the quota tests run remotely.

--- serversidetests/servers/apigeequota.js
+++ serversidetests/servers/apigeequota.js
@@ -3,17 +3,26 @@
  * the Apigee SPI.
  */
 
- var apigeeQuota = require('volos-quota-apigee');
- var config = require('./volos/testconfig/testconfig-apigee');
- var server = require('./volos/quota/test/expressserver');
+var _ = require('underscore');
 
- // Make doubly sure that we are using apigee-access only
- delete config.config.uri;
- delete config.config.key;
+var apigeeQuota = require('volos-quota-apigee');
+var config = require('./volos/testconfig/testconfig-apigee');
+var server = require('./volos/quota/test/expressserver');
 
- var quota = apigeeQuota.create(config.config);
+// Make doubly sure that we are using apigee-access only
+delete config.config.uri;
+delete config.config.key;
 
- // Build an Express server using the code from the cache module
- var app = server(quota);
+var opts = {
+  timeUnit: 'minute',
+  interval: 1,
+  allow: 2
+};
+_.extend(opts, config.config);
 
- app.listen(process.env.PORT || 9002);
+var quota = apigeeQuota.create(opts);
+
+// Build an Express server using the code from the cache module
+var app = server(quota);
+
+app.listen(process.env.PORT || 9002);
