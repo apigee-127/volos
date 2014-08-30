e1f4d973e97adeba4d38bdb7226ca741d12526ed
Dibyo Mukherjee <dmukherjee@apigee.com>
2014-08-29 17:43:38 -0700

    Use user-supplied options to create Analytics

--- analytics/analytics.js
+++ analytics/analytics.js
@@ -3,21 +3,33 @@
 var _ = require('underscore');
 
 
-function Analytics(Spi) {
+function Analytics(Spi, options) {
 	this.Spi = Spi;
-	this.recordsQueue = []; 
+	this.recordsQueue = [];
+	this.recordLimit = options.recordLimit;
+	this.flushLimit = options.flushLimit;
+	this.interval = options.interval;
 }
 module.exports = Analytics;
 
-Analytics.prototype.useAnalytics = function(record) {
-	this.recordsQueue.push(record);
-	
-	console.log(this.recordsQueue.length);
-	
-	if(this.recordsQueue.length % 2 == 0) {
-		this.Spi.useAnalytics(this.recordsQueue, 2)
+Analytics.prototype.useAnalytics = function(record, cb) {
+	if(this.recordsQueue.length < this.recordLimit) {
+		console.log("Pushing data");
+		this.recordsQueue.push(record);
+		console.log("Queue length: " + this.recordsQueue.length);
 	}
-	
+	if(this.recordsQueue.length % this.interval == 0) {
+		this.flush();
+	}
+};
+
+Analytics.prototype.flush = function() {
+	var self = this;
+	this.Spi.useAnalytics(this.recordsQueue, function(err, result) {
+			if(err) { console.log(err) };
+			self.recordsQueue.splice(0,result.accepted);
+			console.log(result.accepted + " elements uploaded");
+		});
 };
 
 Analytics.prototype.expressMiddleWare = function() {
--- analytics/apigeeanalytics.js
+++ analytics/apigeeanalytics.js
@@ -1,18 +1,45 @@
 'use strict';
 
 var Analytics = require('./analytics.js');
+var superagent = require('superagent');
 
-var create = function() {
-	var spi = new ApigeeAnalyticsSpi();
-	return new Analytics(spi);
+var create = function(options) {
+  var spi = new ApigeeAnalyticsSpi(options);
+  return new Analytics(spi, options);
 };
 module.exports.create = create;
 
-var ApigeeAnalyticsSpi = function() {
-	this.something = "Something";
+var ApigeeAnalyticsSpi = function(options) {
+  if (!options.uri) {
+    throw new Error('uri parameter must be specified');
+  }
+  if (!options.key) {
+    throw new Error('key parameter must be specified');
+  }
+  this.uri = options.uri;
+  this.key = options.key;
+  //See if analytics is allowed or not
 };
 
-ApigeeAnalyticsSpi.prototype.useAnalytics = function(recordsQueue, numUpload) {
-	var recordsToBeUploaded = recordsQueue.slice(0, numUpload);
-	console.log(recordsToBeUploaded);
+ApigeeAnalyticsSpi.prototype.useAnalytics = function(recordsQueue, cb) {
+  var recordsToBeUploaded = {};
+  recordsToBeUploaded.records = recordsQueue;
+  
+  superagent.agent().
+  post(this.uri + 'v2/analytics/accept').
+  set('x-DNA-Api-Key', this.key).
+  set('Content-Type', 'application/json').
+  send(JSON.stringify(recordsToBeUploaded)).
+  end(function(err, resp) {
+    if(err) {
+      cb(err)
+    } else {
+      if(resp.statusCode != 200) {
+        var errString = resp.statusCode + ": " + resp.body;
+        cb(errString);
+      } else {
+        cb(undefined, resp.body);
+      }
+    }
+  });
 };
