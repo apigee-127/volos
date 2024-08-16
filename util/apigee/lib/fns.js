3fcda0dfe2ed7798c9a66a6778c0fa64b9312c95
Nitesh Gupta <guptanitesh40@gmail.com>
2024-08-16 15:17:16 +0530

    352446757 Optimize volos-util-apigee package

--- util/apigee/lib/fns.js
+++ util/apigee/lib/fns.js
@@ -1,21 +1,3 @@
-const fns = [
-        'listProxies', 'listSharedflows', 'listDeployments',
-        'undeploy', 'fetchProxy', 'deployProxy', 'delete',
-        'deployNodeApp', 'deployHostedTarget', 'getLogs',
-        'createCache', 'deleteCache', 'getCache', 'listCaches', 'clearCache',
-        'createProduct', 'deleteProduct',
-        'createDeveloper', 'deleteDeveloper',
-        'createApp', 'createAppKey', 'deleteApp',
-        'createTargetServer', 'getTargetServer', 'deleteTargetServer',
-        'listTargetServers', 'updateTargetServer',
-        'createKVM', 'deleteKVM', 'getKVMentry', 'getKVMmap',
-        'addEntryToKVM', 'deleteKVMentry', 'updateKVMentry',
-        'deploySharedflow', 'undeploySharedflow', 'fetchSharedflow',
-        'listSharedflowDeployments', 'deleteSharedflow',
-        'attachFlowHook', 'detachFlowHook', 'getFlowHook',
-        'listRoles', 'getRole', 'createRole', 'deleteRole',
-        'getRolePermissions', 'setRolePermissions',
-        'assignUserRole', 'removeUserRole', 'verifyUserRole', 'listRoleUsers'
-      ];
+const fns = ["listDeployments", "deployProxy", "createProduct", "deleteProduct"];
 
 module.exports = fns;
